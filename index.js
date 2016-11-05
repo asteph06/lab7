7da23ee7627340a3cecdab57e25f587691af8eab
asteph06 <ashtoncaulfieldstephens@gmail.com>
2016-11-04 21:48:17 -0400

    hello world

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 var requests = require('requests');
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 
 console.log("hello world!!!");
 
